9a025a0c7a1d52904e535804c0ab7fe1d8d40a36
Anders-o-n <jvsout@gmail.com>
2023-06-23 00:46:15 +0700

    Имплементируйте модели Role и User интерфейсами GrantedAuthority и UserDetails соответственно.

--- src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
@@ -1,11 +1,13 @@
 package ru.kata.spring.boot_security.demo.entities;
 
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,6 +35,10 @@ public class Role {
     @Override
     public String toString() {
         return this.name;
+    }
+    @Override
+    public String getAuthority() {
+        return getName();
     }
 
 }
--- src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
@@ -1,13 +1,16 @@
 package ru.kata.spring.boot_security.demo.entities;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.Collection;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
@@ -77,6 +80,30 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 
